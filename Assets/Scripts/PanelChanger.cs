20520b9d8ec1f408913a8ef246efbf4d020a3ea4
Rnxra72 <b00134106@mytudublin.ie>
2023-03-04 16:15:34 +0000

    fixed visibility error
    fixed the visibility error of the gameplay panel, now it should appear and disappear on the "a" button click of the left hand controller

--- Assets/Scripts/PanelChanger.cs
+++ Assets/Scripts/PanelChanger.cs
@@ -16,7 +16,7 @@ public class PanelChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")){
+        if(Input.GetButtonDown("Fire3")){
             isScreenVisible = !isScreenVisible;
             uiScreen.SetActive(isScreenVisible);
         }
