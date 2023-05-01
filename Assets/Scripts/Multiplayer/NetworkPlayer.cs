800b8bea6daaa39a67dc68966944a9ab39c28112
tobyfast <b00137691@mytudublin.ie>
2023-05-01 22:11:08 +0100

    up we go

--- Assets/Scripts/Multiplayer/NetworkPlayer.cs
+++ Assets/Scripts/Multiplayer/NetworkPlayer.cs
@@ -4,45 +4,59 @@ using UnityEngine;
 using UnityEngine.XR;
 using Photon.Pun;
 using UnityEngine.XR.Interaction.Toolkit;
+
 public class NetworkPlayer : MonoBehaviour
 {
     public Transform head;
-    public Transform lefthand;
-    public Transform righthand;
+    public Transform leftHand;
+    public Transform rightHand;
+
+
     private PhotonView photonView;
 
     private Transform headRig;
     private Transform leftHandRig;
     private Transform rightHandRig;
 
+
+    // Start is called before the first frame update
     void Start()
     {
-
         photonView = GetComponent<PhotonView>();
+
         XRRig rig = FindObjectOfType<XRRig>();
         headRig = rig.transform.Find("Camera Offset/Main Camera");
         leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
         rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+
+        if (photonView.IsMine)
+        {
+            foreach (var item in GetComponentsInChildren<Renderer>())
+            {
+                item.enabled = false;
+            }
+        }
     }
+
+    // Update is called once per frame
     void Update()
     {
         if (photonView.IsMine)
         {
-            righthand.gameObject.SetActive(false);
-            lefthand.gameObject.SetActive(false);
-            head.gameObject.SetActive(false);
             MapPosition(head, headRig);
-            MapPosition(lefthand, leftHandRig);
-            MapPosition(righthand, rightHandRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
+
         }
 
     }
 
+ 
+
+
     void MapPosition(Transform target, Transform rigTransform)
     {
-
         target.position = rigTransform.position;
         target.rotation = rigTransform.rotation;
     }
 }
-
